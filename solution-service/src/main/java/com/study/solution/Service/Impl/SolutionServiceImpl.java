65ea9124f6b2f613c2f570bf04a0010d4f11ac05
Danil Vasilev <gbhfns90@gmail.com>
2024-06-16 20:41:35 +0700

    Тест с saveAndFlush

--- solution-service/src/main/java/com/study/solution/Service/Impl/SolutionServiceImpl.java
+++ solution-service/src/main/java/com/study/solution/Service/Impl/SolutionServiceImpl.java
@@ -129,6 +129,7 @@ public class SolutionServiceImpl implements SolutionService {
             solution.setTaskId(taskId);
             solution.setTestIndex(0L);
             solution.setUsername(user.getClaim(USERNAME_CLAIM));
+            solutionRepository.saveAndFlush(solution);
 
             long timeLimit = tests.get(0).getTimeLimit();
 
@@ -301,12 +302,10 @@ public class SolutionServiceImpl implements SolutionService {
 //        return result.toString();
 //    }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional
     protected void runCode(List<TestCaseDto> tests, String code, long timeLimit, Solution solution, Jwt user) throws IOException, CodeCompilationException, CodeRuntimeException, TimeLimitException {
         Path path = Files.createTempDirectory("compile");
         File tempFile = new File(path.toAbsolutePath() + "/Main.java");
-
-        solutionRepository.saveAndFlush(solution);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
             writer.write(code);
