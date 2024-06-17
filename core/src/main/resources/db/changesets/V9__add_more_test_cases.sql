b1b3ced34e1ad74d7b86dca1a109cb139e78220d
3eight70 <gbhfns90@gmail.com>
2024-06-17 21:23:36 +0700

    Добавил больше тестов

---
+++ core/src/main/resources/db/changesets/V9__add_more_test_cases.sql
@@ -0,0 +1,4 @@
+INSERT INTO test_cases (id, index, expected_input, expected_output, task_id)
+VALUES
+    ('c2d3b8bf-b6e8-5hv1-993a-b9a34c932d5e', 1, E'3\naaa\nbbb\nccc', E'YES\nYES\nYES', 'd0fdfe00-26d9-11ef-9e35-0800200c9a66'),
+    ('b8bfc2d3-b6e8-5hv1-993a-c932d5eb9a34', 1, E'3\nabc\nabc\nabc', E'NO\nNO\nNO', 'd0fdfe00-26d9-11ef-9e35-0800200c9a66')
