800809a22a7598f8e90b051fb89586d06c48bf88
3eight70 <gbhfns90@gmail.com>
2024-06-17 21:25:32 +0700

    Обновил тесты

--- core/src/main/resources/db/changesets/V9__add_more_test_cases.sql
+++ core/src/main/resources/db/changesets/V9__add_more_test_cases.sql
@@ -1,4 +1,4 @@
 INSERT INTO test_cases (id, index, expected_input, expected_output, task_id)
 VALUES
-    ('c2d3b8bf-b6e8-5hv1-993a-b9a34c932d5e', 1, E'3\naaa\nbbb\nccc', E'YES\nYES\nYES', 'd0fdfe00-26d9-11ef-9e35-0800200c9a66'),
-    ('b8bfc2d3-b6e8-5hv1-993a-c932d5eb9a34', 1, E'3\nabc\nabc\nabc', E'NO\nNO\nNO', 'd0fdfe00-26d9-11ef-9e35-0800200c9a66')
+    ('0844e54e-fe6d-444e-8487-9c88ac8bfc49', 1, E'3\naaa\nbbb\nccc', E'YES\nYES\nYES', 'd0fdfe00-26d9-11ef-9e35-0800200c9a66'),
+    ('c53804c9-b8b2-4eda-820c-4377daef3154', 1, E'3\nabc\nabc\nabc', E'NO\nNO\nNO', 'd0fdfe00-26d9-11ef-9e35-0800200c9a66')
