ca54b2754b52de04b8fb03ea83f6c67e104d288c
Danil Vasilev <gbhfns90@gmail.com>
2024-06-18 15:10:35 +0700

    Обновил миграции

--- core/src/main/resources/db/changesets/V10__add_more_test_cases.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO test_cases (id, index, expected_input, expected_output, task_id)
-VALUES
-    ('0844e54e-fe6d-444e-8487-9c88ac8bfc49', 2, E'3\naaa\nbbb\nccc', E'YES\nYES\nYES', 'd0fdfe00-26d9-11ef-9e35-0800200c9a66'),
-    ('c53804c9-b8b2-4eda-820c-4377daef3154', 3, E'3\nabc\nabc\nabc', E'NO\nNO\nNO', 'd0fdfe00-26d9-11ef-9e35-0800200c9a66')
