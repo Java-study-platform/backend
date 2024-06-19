4403df52a35bc0caa0bfc16107d9c0856a8792f7
Danil Vasilev <gbhfns90@gmail.com>
2024-06-19 20:31:06 +0700

    обновил миграции

--- core/src/main/resources/db/changesets/V1_0_3__data_update.sql
+++ core/src/main/resources/db/changesets/V1_0_3__data_update.sql
@@ -3,4 +3,4 @@ FROM test_cases
 WHERE id = 'b616c035-3e96-4eec-80e6-6e84da3b1d3f';
 
 INSERT INTO test_cases (id, index, expected_input, expected_output, task_id)
-VALUES ('b616c035-3e96-4eec-80e6-6e84da3b1d3f', 1, E '5\n15', E '20\n', 'c844ad85-f607-414f-af2a-6250a1f488ca');
+VALUES ('b616c035-3e96-4eec-80e6-6e84da3b1d3f', 1, E'5\n15', E'20\n', 'c844ad85-f607-414f-af2a-6250a1f488ca');
