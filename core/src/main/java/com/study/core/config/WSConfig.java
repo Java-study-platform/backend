e321f2974c9fede7333a86852b549c32540590e0
3eight70 <gbhfns90@gmail.com>
2024-06-20 21:45:50 +0700

    logging

--- core/src/main/java/com/study/core/config/WSConfig.java
+++ core/src/main/java/com/study/core/config/WSConfig.java
@@ -1,11 +1,19 @@
 package com.study.core.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -25,4 +33,31 @@ public class WSConfig implements WebSocketMessageBrokerConfigurer {
         config.setApplicationDestinationPrefixes("/app");
         config.enableSimpleBroker("/topic");
     }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        converter.setObjectMapper(objectMapper);
+        converter.setContentTypeResolver(resolver);
+        messageConverters.add(converter);
+        return false;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(polygonWebSocketHandler, "/ws/location")
+                .setAllowedOrigins("*");
+        registry.addHandler(experienceHandler, "ws/experience")
+                .setAllowedOriginPatterns("*");
+        registry.addHandler(levelHandler, "ws/level")
+                .setAllowedOriginPatterns("*");
+        registry.addHandler(friendPositionHandler, "ws/friendPosition")
+                .setAllowedOriginPatterns("*");
+        registry.addHandler(eventHandler, "ws/event")
+                .setAllowedOriginPatterns("*");
+    }
+
 }
