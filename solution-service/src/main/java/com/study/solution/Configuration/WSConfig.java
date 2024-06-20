70c27cadfe6ed4320aad70c795500881b7218b59
3eight70 <gbhfns90@gmail.com>
2024-06-20 20:18:23 +0700

    debug

--- solution-service/src/main/java/com/study/solution/Configuration/WSConfig.java
+++ solution-service/src/main/java/com/study/solution/Configuration/WSConfig.java
@@ -1,5 +1,6 @@
 package com.study.solution.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,10 +9,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Slf4j
 public class WSConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        log.info("Registering stomp endpoints");
         registry
                 .addEndpoint("/ws")
                 .setAllowedOrigins("*")
