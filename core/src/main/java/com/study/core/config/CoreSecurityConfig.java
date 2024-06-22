a403f4895afb841a09044b3bee6fe75e1208ddc3
Danil Vasilev <gbhfns90@gmail.com>
2024-06-22 14:12:06 +0700

    test

--- core/src/main/java/com/study/core/config/CoreSecurityConfig.java
+++ core/src/main/java/com/study/core/config/CoreSecurityConfig.java
@@ -60,6 +60,7 @@ public class CoreSecurityConfig {
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/ws/**").permitAll()
                         .requestMatchers(HttpMethod.GET, GET_TESTS_FOR_SERVICE + "**").anonymous()
                         .requestMatchers(HttpMethod.GET, TEST_CASES + "/**").hasRole("ADMIN")
                         .anyRequest().authenticated());
