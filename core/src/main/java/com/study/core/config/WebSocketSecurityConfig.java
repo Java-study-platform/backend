35d5075b70e5a3bfeafc8992744e94c9e56a19af
Danil Vasilev <gbhfns90@gmail.com>
2024-06-22 11:42:52 +0700

    test

---
+++ core/src/main/java/com/study/core/config/WebSocketSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.study.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages.simpDestMatchers("/ws/**").authenticated();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
