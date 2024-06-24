17a27475ffae22478966d103813eeec2b0cccb7e
3eight70 <gbhfns90@gmail.com>
2024-06-24 12:39:24 +0700

    Добавил зависимость для валидации

--- solution-service/src/main/java/com/study/solution/DTO/Solution/SendTestSolutionRequest.java
+++ solution-service/src/main/java/com/study/solution/DTO/Solution/SendTestSolutionRequest.java
@@ -1,10 +1,14 @@
 package com.study.solution.DTO.Solution;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SendTestSolutionRequest {
     @NotNull
+    @NotBlank
+    @Size(min = 1)
     private String code;
 }
