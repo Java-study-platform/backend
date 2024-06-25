6e402c934962379b689eea9aaa3513b23ae681e9
3eight70 <gbhfns90@gmail.com>
2024-06-25 10:35:49 +0700

    logs

--- core/src/main/java/com/study/core/service/impl/ChatServiceImpl.java
+++ core/src/main/java/com/study/core/service/impl/ChatServiceImpl.java
@@ -83,7 +83,9 @@ public class ChatServiceImpl implements ChatService {
         for (MessageDTO mess :dto){
             for (MessageDTO reply: mess.getReplies()) {
                 log.info("reply: "+ reply.toString());
-                log.info("id: " + reply.getId() + " curReactions: " + reply.getCurrentUserReactions().toString());
+                if (reply.getCurrentUserReactions() != null) {
+                    log.info("id: " + reply.getId() + " curReactions: " + reply.getCurrentUserReactions().toString());
+                }
             }
         }
 
